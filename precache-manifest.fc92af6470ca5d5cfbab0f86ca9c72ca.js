15748dcea0338f5a79591f6cc4619ae613ba84d8
Антон Лучкин <luchkin1988@gmail.com>
2020-09-01 22:50:27 +0300

    Updates

---
+++ precache-manifest.fc92af6470ca5d5cfbab0f86ca9c72ca.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceec7d02fe14e2111d5cbb7d26fdc136",
+    "url": "/goit-react-hw--1-components/index.html"
+  },
+  {
+    "revision": "e89d2d6cbfb6a5aaeba2",
+    "url": "/goit-react-hw--1-components/static/css/2.3d72dcb8.chunk.css"
+  },
+  {
+    "revision": "d7be7665b1bcc103712d",
+    "url": "/goit-react-hw--1-components/static/css/main.5b2229f1.chunk.css"
+  },
+  {
+    "revision": "e89d2d6cbfb6a5aaeba2",
+    "url": "/goit-react-hw--1-components/static/js/2.4cb585a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw--1-components/static/js/2.4cb585a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7be7665b1bcc103712d",
+    "url": "/goit-react-hw--1-components/static/js/main.27f8ec6c.chunk.js"
+  },
+  {
+    "revision": "881db68a71ec648e3f21",
+    "url": "/goit-react-hw--1-components/static/js/runtime-main.e9659f5f.js"
+  },
+  {
+    "revision": "25e7ec9de59af098efaace8cb8d1646a",
+    "url": "/goit-react-hw--1-components/static/media/Proxima Nova Light.25e7ec9d.ttf"
+  },
+  {
+    "revision": "43061a86f2c236fa555f1f9d39cec448",
+    "url": "/goit-react-hw--1-components/static/media/Proxima Nova Light.43061a86.woff"
+  },
+  {
+    "revision": "618eab4981d4d3d04cd3da818dfb0d01",
+    "url": "/goit-react-hw--1-components/static/media/Proxima Nova Light.618eab49.svg"
+  },
+  {
+    "revision": "8ba5a276feb95e64692d30c372061c1e",
+    "url": "/goit-react-hw--1-components/static/media/Proxima Nova Light.8ba5a276.eot"
+  },
+  {
+    "revision": "09b9070a79dc9334ad24841d9234dc7c",
+    "url": "/goit-react-hw--1-components/static/media/Proxima Nova Regular.09b9070a.svg"
+  },
+  {
+    "revision": "267b2dfbdadacf55be4d04a731652f71",
+    "url": "/goit-react-hw--1-components/static/media/Proxima Nova Regular.267b2dfb.ttf"
+  },
+  {
+    "revision": "8ef941424f0cfea43ec19b1dc27ecebb",
+    "url": "/goit-react-hw--1-components/static/media/Proxima Nova Regular.8ef94142.woff"
+  },
+  {
+    "revision": "ae4263fb30c97d1a1ffc3d4b7b0cc553",
+    "url": "/goit-react-hw--1-components/static/media/Proxima Nova Regular.ae4263fb.eot"
+  },
+  {
+    "revision": "9b1fada34752bb745b11f406a79a10f2",
+    "url": "/goit-react-hw--1-components/static/media/Proxima Nova Semibold.9b1fada3.ttf"
+  },
+  {
+    "revision": "a80586b00fca79e572f0aa6ed67f0fac",
+    "url": "/goit-react-hw--1-components/static/media/Proxima Nova Semibold.a80586b0.woff"
+  },
+  {
+    "revision": "bfc6fa6cd88cfa6bb0db73aab6b30bed",
+    "url": "/goit-react-hw--1-components/static/media/Proxima Nova Semibold.bfc6fa6c.eot"
+  },
+  {
+    "revision": "ecf246b44599e6480964672b9381b1b3",
+    "url": "/goit-react-hw--1-components/static/media/Proxima Nova Semibold.ecf246b4.svg"
+  },
+  {
+    "revision": "0b6af8e7abe3a04371279203a33c7465",
+    "url": "/goit-react-hw--1-components/static/media/helveticaneuecyr-bold.0b6af8e7.ttf"
+  },
+  {
+    "revision": "0f0dd8bb688f0dfad43ce1b52218d616",
+    "url": "/goit-react-hw--1-components/static/media/helveticaneuecyr-bold.0f0dd8bb.woff"
+  },
+  {
+    "revision": "7ac1c8f0b974a943aa67859c07f86bd2",
+    "url": "/goit-react-hw--1-components/static/media/helveticaneuecyr-bold.7ac1c8f0.svg"
+  },
+  {
+    "revision": "f0fdc06ddffa096c38351904f8e51fab",
+    "url": "/goit-react-hw--1-components/static/media/helveticaneuecyr-bold.f0fdc06d.eot"
+  },
+  {
+    "revision": "2127aa6d0680b144a3bd8be9debeba09",
+    "url": "/goit-react-hw--1-components/static/media/helveticaneuecyr-light.2127aa6d.svg"
+  },
+  {
+    "revision": "abbeee1b9b44e576eb848b081f0c62c6",
+    "url": "/goit-react-hw--1-components/static/media/helveticaneuecyr-light.abbeee1b.ttf"
+  },
+  {
+    "revision": "cc724b3d2f335dbceaf1b896b3aca074",
+    "url": "/goit-react-hw--1-components/static/media/helveticaneuecyr-light.cc724b3d.eot"
+  },
+  {
+    "revision": "ef8af51166fdea31f08d60a7d964bf4b",
+    "url": "/goit-react-hw--1-components/static/media/helveticaneuecyr-light.ef8af511.woff"
+  }
+]);
